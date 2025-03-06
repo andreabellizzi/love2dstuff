eb2d09c8efccc68b3de87e89f99ae123fadf38c9
Andrea Bellizzi <bellizzian@gmail.com>
2025-03-06 12:00:28 +0100

    Update clock.lua with hour offset lut

--- clock.lua
+++ clock.lua
@@ -1,36 +1,51 @@
 function love.load()
-	circle_center_x = 350
-	circle_center_y = 350
-	circle_rad = 150
+    circle_center_x = 350
+    circle_center_y = 350
+    circle_rad = 150
+    hour_offset_lut = {
+        [12]  = {-7, 0},
+        [11]  = { 0, 0},
+        [10]  = { 0, 0},
+        [9]   = { 0, 0},
+        [8]   = { 0, 0},
+        [7]   = { 0, 0},
+        [6]   = { 0, 0},
+        [5]   = { 0, 0},
+        [4]   = { 0, 0},
+        [3]   = { 0, 0},
+        [2]   = {-5, 0},
+        [1]   = { 0, 2},
+        [0]   = { 0, 0}
+    }
 end
 
 
 function draw_hour()
-	love.graphics.line(circle_center_x, circle_center_y, circle_center_x, circle_center_y-circle_rad)
-	local d = circle_rad
-	local xoff = circle_center_x-7
-	local yoff = circle_center_y
-	local x = circle_center_x-7
-	local y = circle_center_y-circle_rad
-	local h = 3
-	for i = 1,12 do		
-		x = circle_rad*math.cos((i-1)*(2*math.pi)/12)
-		y = circle_rad*math.sin((i-1)*(2*math.pi)/12)
-		love.graphics.printf(tostring(h), xoff+x, yoff+y, 300)
-		h = (h+1)%12
-		if h == 0 then h = 12 end
-	end
+    love.graphics.line(circle_center_x, circle_center_y, circle_center_x, circle_center_y-circle_rad)
+    local d = circle_rad
+    local xoff = circle_center_x-7
+    local yoff = circle_center_y
+    local x = circle_center_x-7
+    local y = circle_center_y-circle_rad
+    local h = 3
+    for i = 1,12 do        
+        x = circle_rad*math.cos((i-1)*(2*math.pi)/12)
+        y = circle_rad*math.sin((i-1)*(2*math.pi)/12)
+        love.graphics.printf(tostring(h), xoff+x, yoff+y, 300)
+        h = (h+1)%12
+        if h == 0 then h = 12 end
+    end
 end
 
 function love.draw()
-	love.graphics.printf(
-		string.format("FPS: %f\nOS: %s Ver: %d.%d.%d\nScreen: %d x %d",
-			love.timer.getFPS(),
-			love.system.getOS(), love._version_major, love._version_minor, love._version_revision,
-			love.graphics.getWidth(), love.graphics.getHeight()
-		), love.graphics.getWidth() - 305, 3, 300, 'right')
+    love.graphics.printf(
+        string.format("FPS: %f\nOS: %s Ver: %d.%d.%d\nScreen: %d x %d",
+            love.timer.getFPS(),
+            love.system.getOS(), love._version_major, love._version_minor, love._version_revision,
+            love.graphics.getWidth(), love.graphics.getHeight()
+        ), love.graphics.getWidth() - 305, 3, 300, 'right')
 
-	love.graphics.circle('line', circle_center_x , circle_center_y , circle_rad , 50)
-	draw_hour()
+    love.graphics.circle('line', circle_center_x , circle_center_y , circle_rad , 50)
+    draw_hour()
 
 end
